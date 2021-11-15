6052445a32d80b91771cd3851429f5db8285a513
Dara Yve <darayve@gmail.com>
2021-11-15 18:01:03 -0300

    Implemented enemy patrol script

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private Transform castPosition;
+    [SerializeField] private float moveSpeed = 3;
+    [SerializeField] private float baseCastDistance;
+    const string LEFT = "left";
+    const string RIGHT = "right";
+    [SerializeField] private string facingDirection;
+    private Vector3 baseScale;
+
+    private void Start()
+    {
+        baseScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float velocityX = moveSpeed;
+
+        if (facingDirection == LEFT)
+        {
+            velocityX = -moveSpeed;
+        }
+
+        rb.velocity = new Vector2(velocityX, rb.velocity.y);
+        
+        if (IsTouchingWall() || IsCloseToEdge())
+        {
+            ChangeWalkingDirection();
+        }
+    }
+
+    private void ChangeWalkingDirection()
+    {
+        if (facingDirection == LEFT)
+        {
+            ChangeDirection(RIGHT);
+        }
+        else
+        {
+            ChangeDirection(LEFT);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(Constants.ENEMY_TAG))
+        {
+            ChangeWalkingDirection();
+        }
+    }
+
+    private void ChangeDirection(string newDir)
+    {
+        Vector3 newScale = baseScale;
+        if (facingDirection == LEFT)
+        {
+            newScale.x = -baseScale.x;
+        } else
+        {
+            newScale.x = baseScale.x;
+        }
+
+        transform.localScale = newScale;
+        facingDirection = newDir;
+    }
+
+    private bool IsTouchingWall()
+    {
+        bool value;
+        float castDistance = baseCastDistance;
+
+        if (facingDirection == LEFT)
+        {
+            castDistance = -baseCastDistance;
+        }
+
+        Vector3 targetPosition = castPosition.position;
+        targetPosition.x += castDistance;
+
+        Debug.DrawLine(castPosition.position, targetPosition, Color.blue);
+
+        if (Physics2D.Linecast(castPosition.position, targetPosition, 1 << LayerMask.NameToLayer(Constants.GROUND_LAYER))) {
+            value = true;
+        } else
+        {
+            value = false;
+        }
+
+        return value;
+    }
+
+    private bool IsCloseToEdge()
+    {
+        bool value;
+        float castDistance = baseCastDistance;
+
+        Vector3 targetPosition = castPosition.position;
+        targetPosition.y -= castDistance;
+
+        Debug.DrawLine(castPosition.position, targetPosition, Color.yellow);
+        
+        if (Physics2D.Linecast(castPosition.position, targetPosition, 1 << LayerMask.NameToLayer(Constants.GROUND_LAYER)))
+        {
+            value = false;
+        }
+        else
+        {
+            value = true;
+        }
+
+        return value;
+    }
+}
