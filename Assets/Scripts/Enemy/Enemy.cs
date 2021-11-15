3fd861a2ad0c96bd007cd9996f47eaa6665aa518
Dara Yve <darayve@gmail.com>
2021-11-15 18:12:54 -0300

    Organized variables

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     [SerializeField] private Transform castStart;
-    public float visionRange = 4;
 
-    public enum EnemyState { idle, running, attack }
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    protected Collider2D coll;
     protected EnemyState state;
+    protected bool wasPlayerDetected = false;
+    protected bool isSearchingForPlayer = false;
+
+    public bool isFacingLeft;
+    public float visionRange = 6;
+    public float moveSpeed = 2;
+    public enum EnemyState { idle, running, attack }
 
     public abstract void Attack();
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
 
     public bool DetectPlayer(float distance)
     {
