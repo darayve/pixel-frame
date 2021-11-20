78220022096edc6d88d12e8cfa5273683a92dc14
Dara Yve <darayve@gmail.com>
2021-11-19 22:40:16 -0300

    Created script to spawn clouds and make them float

---
+++ Assets/Scripts/Decoration/SpawnClouds.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClouds : MonoBehaviour
+{
+    [SerializeField] private GameObject[] clouds;
+    [SerializeField] private float spawnInterval;
+    [SerializeField] GameObject end;
+    private Vector2 start;
+    private void Start()
+    {
+        start = transform.position;
+        //StartCoroutine(Spawn());
+        PreSpawn();
+        StartCoroutine(DoSpawn());
+    }
+
+    private void PreSpawn()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Vector2 spawningPosition = start + Vector2.right * (i * 2);
+            Spawn(start);
+        }
+    }
+
+    //private IEnumerator Spawn()
+    private void Spawn(Vector2 startPosition)
+    {
+        //yield return new WaitForSeconds(spawnInterval);
+
+        int index = Random.Range(0, clouds.Length);
+        float randSpeed = Random.Range(0.5f, 1.5f);
+        float randScale = Random.Range(0.3f, 1.2f);
+        float randStartY = Random.Range(start.y - 2f, start.y + 2f);
+        GameObject cloud = Instantiate(clouds[index]);
+        
+
+        cloud.transform.localScale = new Vector2(randScale, randScale);
+        cloud.transform.position = new Vector2(start.x, randStartY);
+        Cloud.StartSpawning(randSpeed, end.transform.position.x);
+
+        //StartCoroutine(Spawn());
+    }
+
+    private IEnumerator DoSpawn()
+    {
+        yield return new WaitForSeconds(spawnInterval);
+        Spawn(start);
+        StartCoroutine(DoSpawn());
+    }
+}
